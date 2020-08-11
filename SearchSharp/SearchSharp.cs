9a1c223d5ec04a96765bfd87f34d1ee75ce9305e
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2020-08-11 10:52:49 +0100

    Initial commit

---
+++ SearchSharp/SearchSharp.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace SearchSharp
+{
+    public static class SearchSharp<T>
+    {
+        /// <summary>
+        /// Returns the index of the given item. Index includes the number 0, for example the 1st items index is 0, and so on.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static int Linear(T item, T[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (item.Equals(data[i]))
+                {
+                    return i;
+                }
+            }
+
+            throw new Exception("That item is not in the dataset.");
+        }
+
+        /// <summary>
+        /// Returns the index of the given item using the Binary Search algorithm.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="data"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static int Binary(int item, int[] data, int min, int max)
+        {
+            if (min >= max)
+            {
+                throw new Exception("That item is not in the dataset.");
+            }
+
+            int mid = (max + min) / 2;
+
+            if (data[mid] < item)
+            {
+                return Binary(item, data, mid + 1, max);
+            }
+            else if (item < data[mid])
+            {
+                return Binary(item, data, min, mid - 1);
+            }
+            else
+            {
+                return mid;
+            }
+        }
+
+        /// <summary>
+        /// Performs a Jump Search on the given array, and returns the index of the item to search for.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="data"></param>
+        /// <param name="block"></param>
+        /// <returns></returns>
+        public static int Jump(int item, int[] data, int block)
+        {
+            int goBack = 0;
+
+            for (int i = 0; i < data.Length; i += block)
+            {
+                if (item < data[i])
+                {
+                    goBack = i - block;
+                    break;
+                }
+            }
+
+            for (int j = goBack; j < data.Length; j++)
+            {
+                if (item == data[j])
+                {
+                    return j;
+                }
+            }
+
+            throw new Exception("That item is not in the dataset.");
+        }
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using SearchSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] data = new int[10]
+            {
+                1,
+                3,
+                5,
+                6,
+                7,
+                10,
+                12,
+                14,
+                34,
+                89,
+            };
+
+            Console.WriteLine("Binary Search: " + SearchSharp<int>.Binary(89, data, 0, data.Length));
+            Console.WriteLine("Linear Search: " + SearchSharp<int>.Linear(89, data));
+            Console.WriteLine("Jump Search: " + SearchSharp<int>.Jump(89, data, 4));
+            Console.ReadLine();
+
+        }
+    }
+}
