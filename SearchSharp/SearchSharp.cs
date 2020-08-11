646678ba49509e5c2d51a814a5c23a1a992919fb
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2020-08-11 11:06:23 +0100

    Added comments

--- SearchSharp/SearchSharp.cs
+++ SearchSharp/SearchSharp.cs
@@ -12,14 +12,17 @@ namespace SearchSharp
         /// <returns></returns>
         public static int Linear(T item, T[] data)
         {
+            // Go through the array
             for (int i = 0; i < data.Length; i++)
             {
+                // If the item is found, return the index that it was found at
                 if (item.Equals(data[i]))
                 {
                     return i;
                 }
             }
 
+            // If we reach this part, we know the item does not exist
             throw new Exception("That item is not in the dataset.");
         }
 
@@ -33,15 +36,18 @@ namespace SearchSharp
         /// <returns></returns>
         public static int Binary(int item, int[] data, int min, int max)
         {
+            // If minimum ends up being bigger than the maximum, then we know the item does not exist
             if (min >= max)
             {
                 throw new Exception("That item is not in the dataset.");
             }
 
+            // Calculate the midpoint
             int mid = (max + min) / 2;
 
             if (data[mid] < item)
             {
+                // If the middle item is smaller than the item to be found, then call the function again with modded values
                 return Binary(item, data, mid + 1, max);
             }
             else if (item < data[mid])
@@ -63,10 +69,13 @@ namespace SearchSharp
         /// <returns></returns>
         public static int Jump(int item, int[] data, int block)
         {
+            // Used for when we need to go back to a certain part of the array
             int goBack = 0;
 
+            // Go through the array, incrementing by the value given in the block arg
             for (int i = 0; i < data.Length; i += block)
             {
+                // If the item is less than the current element, then we know we have to go back by a certain amount
                 if (item < data[i])
                 {
                     goBack = i - block;
@@ -74,6 +83,7 @@ namespace SearchSharp
                 }
             }
 
+            // Simple linear search.
             for (int j = goBack; j < data.Length; j++)
             {
                 if (item == data[j])
@@ -82,6 +92,7 @@ namespace SearchSharp
                 }
             }
 
+            // If we get here we know the item does not exist
             throw new Exception("That item is not in the dataset.");
         }
     }
